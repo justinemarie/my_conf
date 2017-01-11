4cbf8ab5bca3e29d23242c3fe1649ee990434f1f
Justine Sherry <stinie@gmail.com>
2017-01-11 11:53:13 -0800

    first commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This is my personal script to set up everything the way I want it.
+
+# REBOOT SCRIPTS PETER
+sudo mv /sbin/reboot /sbin/reboot2
+sudo cp reboot /sbin/reboot
+
+## Packages
+sudo apt-get update
+sudo apt-get install ruby subversion build-essential libpcap0.8-dev vim cmake perl lvm2 python-software-properties python-dev ethtool curl colormake
+
+sudo gem install trollop
+
+wget http://ovh.dl.sourceforge.net/sourceforge/ibmonitor/ibmonitor-1.4.tar.gz
+tar -xf ibmonitor-1.4.tar.gz
+
+## Set up my keys
+cp id_rsa.pub ~/.ssh/authorized_keys
+
+## Bashrc, vimrc, bash profile
+cp bashrc ~/.bashrc
+cp vimrc ~/.vimrc
+cp bash_profile ~/.bash_profile
+
+## Set up git
+git config --global user.name "Justine Sherry"
+git config --global user.email sherry@nefeli.io
+git config --global core.editor vim
+
---
+++ vimrc
@@ -0,0 +1,17 @@
+set nocompatible
+
+set statusline=%<\ %n:%f\ %m%r%y%=%-35.(line:\ %l\ of\ %L,\ col:\ %c%V\ (%P)%)
+filetype plugin indent on
+ 
+syntax on
+set number
+set showmatch
+set ignorecase
+set autoindent
+set history=1000
+set cursorline
+set expandtab
+set shiftwidth=2
+set tabstop=2
+set softtabstop=2
+set foldmethod=syntax
